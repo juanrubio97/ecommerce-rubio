715d4a1f4103a724842ca81f0224b696a376f28f
Juan Cruz Rubio <62122428+juanrubio97@users.noreply.github.com>
2022-06-28 20:51:45 -0300

    Delete src directory

--- src/components/CartWidget/CartWidget.jsx
+++
@@ -1,9 +0,0 @@
-import { BsFillCartFill } from "react-icons/bs";
-
-const CartWidget = () => {
-  return (
-    <BsFillCartFill style={{marginRight:"10px", width:"30px", height:"30px" }}/>
-  )
-}
-
-export default CartWidget
